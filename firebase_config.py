23455f7dc00847c703423f76d4945d9831fe5f9e
Vikas Varma Bedada <vikasvarmabedada@gmail.com>
2026-02-28 05:42:25 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,4 @@
 import firebase_admin
-from firebase_admin import credentials, db, auth
-
-import firebase_admin
 from firebase_admin import credentials, db
 import streamlit as st
 
@@ -15,12 +12,4 @@ def init_firebase():
 
 def get_db_reference(path="/"):
     init_firebase()
-    return db.reference(path)
-
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred, {
-        "databaseURL": "https://mentalmadhilo-6900d-default-rtdb.firebaseio.com/"
-    })
-
-def get_db_reference(path):
     return db.reference(path)
